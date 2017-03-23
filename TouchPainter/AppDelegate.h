e153fff2d045f4ddef402404343547d3445829a2
haodi1 <523918514@qq.com>
2017-03-23 09:48:19 +0800

    Initial Commit

---
+++ TouchPainter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchPainter
+//
+//  Created by adi on 2017/3/23.
+//  Copyright © 2017年 adi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
